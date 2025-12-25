5b8ce3ac684204ada7399f61702f35e48f1fe6d0
Aqiba Abdul Qadir <139615591+AqibaAbdulQadir@users.noreply.github.com>
2025-12-25 14:59:00 +0500

    Refactor external_memory_module  per Vivado Implementation

--- src/external_memory_module.v
+++ src/external_memory_module.v
@@ -1,13 +1,19 @@
-`include "params.vh"
-
-module external_memory_module (
-    input [`EMEM_W_ADDR_WIDTH-1:0] addr, 
-    output [`PIXEL_WIDTH-1:0] pixel
+module external_memory_module #(
+`include "par.vh"
+)(
+    input  [EMEM_W_ADDR_WIDTH-1:0] addr, 
+    output [PIXEL_WIDTH-1:0]        pixel
 );
 
-    reg [`PIXEL_WIDTH-1:0] memory [0:`IMAGE_SIZE-1]; // only to not make memory too large, I instatitae with image size instead of 4GB
+
+    // Simulation-only memory model
+    reg [PIXEL_WIDTH-1:0] memory [0:IMAGE_SIZE-1];
+
     initial begin
-        $readmemh("data//ext_mem.mem", memory);
+        $display("Loading external memory from ext_mem.mem (simulation only)");
+        $readmemh("ext_mem.mem", memory);
     end
+
     assign pixel = memory[addr];
+
 endmodule
