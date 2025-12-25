3f7627dc224135d21bf943bf00ae00578c46dd12
Aqiba Abdul Qadir <139615591+AqibaAbdulQadir@users.noreply.github.com>
2025-12-25 15:00:20 +0500

    Fix syntax for parameterized input and output types

--- src/mux_rb_to_1.v
+++ src/mux_rb_to_1.v
@@ -1,11 +1,13 @@
-module mux_rb_to_1 (
+module mux_rb_to_1 #(
+`include "par.vh"
+) (
     input en,
-    input [`RB_ADDR-1:0] sel,
-    input [`BRAM_R_DATA_WIDTH-1:0] in,
-    output reg [`PIXEL_WIDTH-1:0] out
+    input [RB_ADDR-1:0] sel,
+    input [BRAM_R_DATA_WIDTH-1:0] in,
+    output reg [PIXEL_WIDTH-1:0] out
 );
     always @(*) begin
-        if (en) out = in[sel*`PIXEL_WIDTH +: `PIXEL_WIDTH];
+        if (en) out = in[sel*PIXEL_WIDTH +: PIXEL_WIDTH];
         else out = 0;
     end
     
