d575f0be162ea90927b1176cec2bc352c9b9ae08
Aqiba Abdul Qadir <139615591+AqibaAbdulQadir@users.noreply.github.com>
2025-12-25 15:02:09 +0500

    Refactor top module to use parameters instead of macros per Vivado Implementation

--- src/top.v
+++ src/top.v
@@ -1,10 +1,13 @@
-`include "params.vh"
-
-module top (
+module top #(
+`include "par.vh"
+)(
     input clk,
     input start,
+    output read_allowed,
+    output [EMEM_W_ADDR_WIDTH-1:0] E_MEM_ADDR,
+    input [PIXEL_WIDTH-1:0] data_in_a,
     output complete,
-    output [`K*`PIXEL_WIDTH-1:0] out
+    output [K*PIXEL_WIDTH-1:0] out
 );
 
     wire en_e_mem_addr;
@@ -12,20 +15,36 @@ module top (
     wire en_r_bram_addr;
     wire en_a;
     wire en_b;
-    wire [`EMEM_W_ADDR_WIDTH-1:0] E_MEM_ADDR;
-    wire [`BRAM_ADDR-1:0] BRAM_W;
-    wire [`BRAM_DEPTH_ADDR-1:0] W_BRAM_ADDR;
-    wire [`BRAM_R_ADDR_WIDTH-1:0] R_BRAM_ADDR;
+    
+    wire [BRAM_ADDR-1:0] BRAM_W;
+    wire [BRAM_DEPTH_ADDR-1:0] W_BRAM_ADDR;
+    wire [BRAM_R_ADDR_WIDTH-1:0] R_BRAM_ADDR;
 
 
-    wire [`BRAM_R_DATA_WIDTH-1:0] out_mm;
-    wire [`RB_ADDR-1:0] steer;
-    wire [`PIXEL_WIDTH-1:0] data_in_a;
-    wire [`BRAM_R_DATA_WIDTH-1:0] hold_out;
-    reg [`BRAM_R_DATA_WIDTH-1:0] curr;
+    wire [BRAM_R_DATA_WIDTH-1:0] out_mm;
+    wire [RB_ADDR-1:0] steer;
+   
+    wire [BRAM_R_DATA_WIDTH-1:0] hold_out;
+    reg [BRAM_R_DATA_WIDTH-1:0] curr;
     wire steer_en;
 
-    control_module cm (        
+    control_module #(    
+    .K(K),
+    .RB_DEPTH(RB_DEPTH),
+    .EMEM_W_ADDR_WIDTH(EMEM_W_ADDR_WIDTH),
+    .PIXEL_WIDTH(PIXEL_WIDTH),
+    .IMAGE_SIZE(IMAGE_SIZE),
+    .BRAM_DEPTH_ADDR(BRAM_DEPTH_ADDR),
+    .BRAMs(BRAMs),
+    .RBs(RBs),
+    .TOTAL_DEPTH(TOTAL_DEPTH),
+    .BRAM_W_ADDR_WIDTH(BRAM_W_ADDR_WIDTH),
+    .BRAM_R_ADDR_WIDTH(BRAM_R_ADDR_WIDTH),
+    .BRAM_R_DATA_WIDTH(BRAM_R_DATA_WIDTH),
+    .BRAM_W_DATA_WIDTH(BRAM_W_DATA_WIDTH),
+    .RB_ADDR(RB_ADDR),
+    .BRAM_ADDR(BRAM_ADDR),
+    .IMAGE_ADDR(IMAGE_ADDR)) cm (        
         clk,
         start,
         complete,
@@ -38,7 +57,23 @@ module top (
         steer_en
     );
 
-    address_generator_module agm (
+    address_generator_module #(
+        .K(K),
+    .RB_DEPTH(RB_DEPTH),
+    .EMEM_W_ADDR_WIDTH(EMEM_W_ADDR_WIDTH),
+    .PIXEL_WIDTH(PIXEL_WIDTH),
+    .IMAGE_SIZE(IMAGE_SIZE),
+    .BRAM_DEPTH_ADDR(BRAM_DEPTH_ADDR),
+    .BRAMs(BRAMs),
+    .RBs(RBs),
+    .TOTAL_DEPTH(TOTAL_DEPTH),
+    .BRAM_W_ADDR_WIDTH(BRAM_W_ADDR_WIDTH),
+    .BRAM_R_ADDR_WIDTH(BRAM_R_ADDR_WIDTH),
+    .BRAM_R_DATA_WIDTH(BRAM_R_DATA_WIDTH),
+    .BRAM_W_DATA_WIDTH(BRAM_W_DATA_WIDTH),
+    .RB_ADDR(RB_ADDR),
+    .BRAM_ADDR(BRAM_ADDR),
+    .IMAGE_ADDR(IMAGE_ADDR)) agm (
         clk,
         en_e_mem_addr,
         en_w_bram_addr,
@@ -50,13 +85,23 @@ module top (
         W_BRAM_ADDR,
         R_BRAM_ADDR 
     );
-
-    external_memory_module em(
-        E_MEM_ADDR,
-        data_in_a
-    );
-
-    memory_module mm (
+     
+    memory_module #(    .K(K),
+    .RB_DEPTH(RB_DEPTH),
+    .EMEM_W_ADDR_WIDTH(EMEM_W_ADDR_WIDTH),
+    .PIXEL_WIDTH(PIXEL_WIDTH),
+    .IMAGE_SIZE(IMAGE_SIZE),
+    .BRAM_DEPTH_ADDR(BRAM_DEPTH_ADDR),
+    .BRAMs(BRAMs),
+    .RBs(RBs),
+    .TOTAL_DEPTH(TOTAL_DEPTH),
+    .BRAM_W_ADDR_WIDTH(BRAM_W_ADDR_WIDTH),
+    .BRAM_R_ADDR_WIDTH(BRAM_R_ADDR_WIDTH),
+    .BRAM_R_DATA_WIDTH(BRAM_R_DATA_WIDTH),
+    .BRAM_W_DATA_WIDTH(BRAM_W_DATA_WIDTH),
+    .RB_ADDR(RB_ADDR),
+    .BRAM_ADDR(BRAM_ADDR),
+    .IMAGE_ADDR(IMAGE_ADDR)) mm (
         clk,
         en_a,
         BRAM_W,
@@ -70,13 +115,29 @@ module top (
 
     // assign hold_out[39:32] = data_in_a;
 
-    steer_module sm (
+    steer_module #(    .K(K),
+    .RB_DEPTH(RB_DEPTH),
+    .EMEM_W_ADDR_WIDTH(EMEM_W_ADDR_WIDTH),
+    .PIXEL_WIDTH(PIXEL_WIDTH),
+    .IMAGE_SIZE(IMAGE_SIZE),
+    .BRAM_DEPTH_ADDR(BRAM_DEPTH_ADDR),
+    .BRAMs(BRAMs),
+    .RBs(RBs),
+    .TOTAL_DEPTH(TOTAL_DEPTH),
+    .BRAM_W_ADDR_WIDTH(BRAM_W_ADDR_WIDTH),
+    .BRAM_R_ADDR_WIDTH(BRAM_R_ADDR_WIDTH),
+    .BRAM_R_DATA_WIDTH(BRAM_R_DATA_WIDTH),
+    .BRAM_W_DATA_WIDTH(BRAM_W_DATA_WIDTH),
+    .RB_ADDR(RB_ADDR),
+    .BRAM_ADDR(BRAM_ADDR),
+    .IMAGE_ADDR(IMAGE_ADDR)) sm (
         steer_en,
         steer,
         out_mm, 
         hold_out
     );
     // always @(posedge clk) curr <= hold_out;
-    assign out = (en_e_mem_addr && en_r_bram_addr && !complete)? {data_in_a, hold_out}: {`K*`PIXEL_WIDTH{1'bz}};
+    assign out = (en_e_mem_addr && en_r_bram_addr && !complete)? {data_in_a, hold_out}: {K*PIXEL_WIDTH{1'bz}};
+    assign read_allowed = en_r_bram_addr && en_w_bram_addr;
     // assign out = {data_in_a, hold_out};
 endmodule
