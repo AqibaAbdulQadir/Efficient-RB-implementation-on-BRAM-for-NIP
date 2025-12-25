b7ed9255f0d5deb849381709b76a4d9e0826ea5a
Aqiba Abdul Qadir <139615591+AqibaAbdulQadir@users.noreply.github.com>
2025-12-25 14:59:44 +0500

    Refactor memory_module  per Vivado Implementation

--- src/memory_module.v
+++ src/memory_module.v
@@ -1,34 +1,36 @@
-`include "params.vh"
-
-module memory_module (
+module memory_module #(
+`include "par.vh"
+) (
     input clk,
     input  wire EN_A,
-    input wire [`BRAM_ADDR-1:0] BRAM_W,
-    input  wire [`BRAM_DEPTH_ADDR-1:0] ADDR_A,
-    input  wire [`BRAM_W_DATA_WIDTH-1:0] DIN_A,
+    input  wire [BRAM_ADDR-1:0] BRAM_W,
+    input  wire [BRAM_DEPTH_ADDR-1:0] ADDR_A,
+    input  wire [BRAM_W_DATA_WIDTH-1:0] DIN_A,
 
     input  EN_B,
-    input   [`BRAM_R_ADDR_WIDTH-1:0] ADDR_B,
-    output  [`BRAM_R_DATA_WIDTH-1:0] DOUT_B
+    input   [BRAM_R_ADDR_WIDTH-1:0] ADDR_B,
+    output  [BRAM_R_DATA_WIDTH-1:0] DOUT_B
 );
     
     genvar i;
-    wire [(`BRAMs<<5)-1:0] DOUT;
+    wire [(BRAMs<<5)-1:0] DOUT;
 
     generate
-        for (i = 0; i < `BRAMs; i = i+1) begin : BRAMS
-            BRAM18 bram (
-                .clk(clk),
-                .w_en(EN_A && (BRAM_W == i)),
-                .w_addr(ADDR_A),
-                .w_data(DIN_A),
-                .r_en(EN_B),
-                .r_addr(ADDR_B),
-                .r_data(DOUT[i<<5+: 32])
+        for (i = 0; i < BRAMs; i = i+1) begin : BRAMS
+            BRAM bram (
+                .clka(clk),
+                .clkb(clk),
+                .ena(EN_A && (BRAM_W == i)),
+                .wea(EN_A && (BRAM_W == i)),
+                .addra(ADDR_A),
+                .dina(DIN_A),
+                .enb(EN_B),
+                .addrb(ADDR_B),
+                .doutb(DOUT[i<<5+: 32])
             );
         end
     endgenerate
 
-    assign DOUT_B = DOUT[`BRAM_R_DATA_WIDTH-1:0];
+    assign DOUT_B = DOUT[BRAM_R_DATA_WIDTH-1:0];
 
 endmodule
