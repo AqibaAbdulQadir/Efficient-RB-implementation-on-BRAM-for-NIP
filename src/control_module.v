a893d4a05e9c420d775a7f4d9e887ad8446c98a5
Aqiba Abdul Qadir <139615591+AqibaAbdulQadir@users.noreply.github.com>
2025-12-25 14:57:36 +0500

    Refactor control_module per Vivado Implementation

--- src/control_module.v
+++ src/control_module.v
@@ -1,6 +1,6 @@
-`include "params.vh"
-
-module control_module (
+module control_module #(
+`include "par.vh"
+)(
     input clk,
     input start,
     output reg complete,
@@ -9,16 +9,16 @@ module control_module (
     output reg en_r_bram_addr,
     output reg en_a,
     output reg en_b,
-    output reg [`RB_ADDR-1:0] steer,
+    output reg [RB_ADDR-1:0] steer,
     output reg steer_en
 );
 
-    localparam S_IDLE = 3'b000, S_WRITE = 3'b001, S_READ = 3'b010, S_RW = 3'b011, S_DONE = 3'b100, max_writes = `IMAGE_SIZE-`TOTAL_DEPTH-1;
+    localparam S_IDLE = 3'b000, S_WRITE = 3'b001, S_READ = 3'b010, S_RW = 3'b011, S_DONE = 3'b100, max_writes = IMAGE_SIZE-TOTAL_DEPTH-1;
 
     reg [2:0]  state, next_state;
-    reg [`BRAM_W_ADDR_WIDTH:0] write_cnt; // one cycle wasted in the beginning due to write address generation
-    reg [`IMAGE_ADDR-1:0] rw_cnt;
-    reg [`BRAM_W_ADDR_WIDTH-1:0] buff_add;
+    reg [BRAM_W_ADDR_WIDTH:0] write_cnt; // one cycle wasted in the beginning due to write address generation
+    reg [IMAGE_ADDR-1:0] rw_cnt;
+    reg [BRAM_W_ADDR_WIDTH-1:0] buff_add;
 
     initial begin
         en_e_mem_addr = 0;
@@ -38,7 +38,7 @@ module control_module (
         else begin
             case (state)
                 S_IDLE: next_state = S_WRITE;
-                S_WRITE: if (write_cnt == `TOTAL_DEPTH) next_state = S_READ;
+                S_WRITE: if (write_cnt == TOTAL_DEPTH) next_state = S_READ;
                 S_READ: next_state = S_RW;
                 S_RW: if (rw_cnt == max_writes) next_state = S_DONE;  
             endcase
@@ -55,14 +55,14 @@ module control_module (
                 buff_add <= 0;
             end
             S_WRITE: begin
-                if (write_cnt == `TOTAL_DEPTH) write_cnt <= 0;
+                if (write_cnt == TOTAL_DEPTH) write_cnt <= 0;
                 else write_cnt <= write_cnt + 1;
             end
             S_RW: begin
                 if (rw_cnt == max_writes) rw_cnt <= 0;
                 else begin 
                     rw_cnt <= rw_cnt + 1;
-                    if (buff_add < `TOTAL_DEPTH-1) buff_add <= buff_add + 1;
+                    if (buff_add < TOTAL_DEPTH-1) buff_add <= buff_add + 1;
                     else buff_add <= 0;
                 end
             end
@@ -103,7 +103,7 @@ module control_module (
                 en_b = 1;
                 steer_en = 0;
                 steer = 0;
-                // steer = buff_add / `RB_DEPTH;
+                // steer = buff_add / RB_DEPTH;
             end
             S_RW: begin
                 en_e_mem_addr = 1;
@@ -112,7 +112,7 @@ module control_module (
                 en_a = 1;
                 en_b = 1;
                 steer_en = 1;
-                steer = buff_add / `RB_DEPTH;
+                steer = buff_add / RB_DEPTH;
 
             end
             S_DONE: begin
